a7930006d91fd3a5e4087ddec4fd1f2fb8d2e633
Bigbaldman <128797654+Bigbaldman@users.noreply.github.com>
2023-06-08 16:47:18 +0530

    Updated Script.js
    Made changes in "Script.js".

--- Script.js
+++ Script.js
@@ -1,1 +1,16 @@
+var button = document.createElement("button");
+button.onclick = function() {
+    location.href = 'New page.html';
+  };
+  button.className = "round-rect-button";
+  button.innerHTML = "BUY HERE";
+  document.body.appendChild(button);
+  var button = document.createElement("button");
 
+  button.onclick = function() {
+    location.href = 'New page.html';
+  };
+  button.className = "round-rect-button";
+  button.innerHTML = "BUY HERE";
+  
+  document.body.appendChild(button);
